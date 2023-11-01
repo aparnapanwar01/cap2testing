bc65f5274440755e6758a936cf334029d72105c4
Punit Pawar <punit.panwar@gmail.com>
2023-11-01 22:57:13 +0000

    testing5

---
+++ test/serv1.test.js
@@ -0,0 +1,25 @@
+const cds = require('@sap/cds/lib')
+
+describe('cap/samples - Fiori APIs - v2', function() {
+
+  const { GET, expect, axios } = cds.test (__dirname+'/..')
+  axios.defaults.auth = { username: 'joe', password: 'joe' }
+
+  // if (this.timeout) this.timeout(1e6)
+/*
+  it('serves $metadata documents in v2', async () => {
+    const { headers, data } = await GET `/odata/v2/browse/$metadata`
+    expect(headers).to.contain({
+      'content-type': 'application/xml',
+      'dataserviceversion': '2.0',
+    })
+    //expect(data).to.contain('<EntitySet Name="GenreHierarchy" EntityType="CatalogService.GenreHierarchy"/>')
+  })*/
+
+  it('Sales data ', async () => {
+    const { data } = await GET `/catalog/Sales`
+   // expect(data).to.containSubset({d:{results:[]}})
+    expect(data.value.length).to.be.greaterThanOrEqual(5)
+  })
+
+})
